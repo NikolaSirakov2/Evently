48102f180449076ffdd76d57a97152de1560f2db
NikolaSirakov2 <nikolasirakov1@gmail.com>
2023-12-26 20:50:23 +0700

    Add Clerk log in verification

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='wrapper flex items-center justify-between'>Copyrights December 2023</div>
+    <div className='wrapper flex items-center justify-between'>Copyrights December 2023 @</div>
   )
 }
 
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,7 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { Button } from "../ui/button"
 
 function Header() {
   return (
@@ -9,7 +11,16 @@ function Header() {
                 <Image src="/assets/images/logo.svg" width={128} height={38} alt="logo"/>
             </Link>
             <div className="flex w-32 justify-end gap-3">
-
+                <SignedIn>
+                    <UserButton afterSignOutUrl="/"></UserButton>
+                </SignedIn>
+                <SignedOut>
+                    <Button className="rounded-full" size="lg">
+                        <Link href="/sign-in">
+                            Login
+                        </Link>
+                    </Button>
+                </SignedOut>
             </div>
         </div>
     </header>
