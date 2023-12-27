4c3cd6fb976da0131b355687f59df33479dbaccd
NikolaSirakov2 <nikolasirakov1@gmail.com>
2023-12-27 21:52:44 +0700

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-    </main>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Host and Connect your Events</h1>
+            <p className="p-regular-20 md:p-regular-24">
+               Join our community of developers and create and manage your online evenets here!
+            </p>
+            <Button size={"lg"} asChild className="button w-full sm:w-fit">
+              <Link href="#events">
+                Check our products
+              </Link>
+            </Button>
+          </div>
+
+          <Image 
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000} 
+            className="max-h-[70vh] object-contain object-center 2xl:mah-h-[50vh]"
+            />
+        </div>
+      </section>
+
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+          <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+          <div className="flex w-full flex-col gap-5 md:flex-row">
+              Search 
+              CategoryFilter
+        </div>
+      </section>
+    </>
   )
 }
