2133fe42048e77d52b98e0834ed7b3e516983d76
NikolaSirakov2 <nikolasirakov1@gmail.com>
2023-12-27 21:58:01 +0700

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Link } from "lucide-react";
+import Link from "next/link"
 import Image from "next/image";
 
 export default function Home() {
